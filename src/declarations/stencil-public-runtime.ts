d67bffdf6700290897f98926b29410fc061f9331
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-10-17 07:29:02 +0200

    fix: restore HTMLStencilElement autocorrect compatibility

--- src/declarations/stencil-public-runtime.ts
+++ src/declarations/stencil-public-runtime.ts
@@ -395,10 +395,8 @@ export declare function forceUpdate(ref: any): void;
  */
 export declare function getRenderingRef(): any;
 
-export interface HTMLStencilElement extends Omit<HTMLElement, 'autocorrect'> {
+export interface HTMLStencilElement extends HTMLElement {
   componentOnReady(): Promise<this>;
-  // TODO: remove this when [added to typescript](https://github.com/microsoft/typescript/issues/62083)
-  autocorrect: 'on' | 'off';
 }
 
 /**
